a1963c8822740568ce31dfeee11d967ca6a72b3b
EgorovM <EgorovM@bitbucket.org>
2021-03-29 15:52:41 +0300

    use sqlite as local

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,3 +5,9 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': root('db.sqlite3'),
+    }
+}
