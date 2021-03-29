c4e39a1c764ad29262858b27e92af2ecbb7daa43
EgorovM <EgorovM@bitbucket.org>
2021-03-29 15:04:40 +0300

    update database

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,9 +5,17 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+INSTALLED_APPS += [
+    'django.contrib.postgres'
+]
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
